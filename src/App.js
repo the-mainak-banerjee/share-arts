58d170bc25add769f9deef34a5f901fcc4bbd939
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-08-09 12:24:40 +0530

    created basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import CreatePost from "./pages/CreatePost";
+import Home from "./pages/Home";
+import PostDetails from "./pages/PostDetails";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      Hi 
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile/:userId' element={<Profile/>}/>
+      <Route path='/posts/:postId' element={<PostDetails/>}/>
+      <Route path='/createPost' element={<CreatePost/>}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/CreatePost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <div>
+      Crete Post
+    </div>
+  )
+}
+
+export default CreatePost
